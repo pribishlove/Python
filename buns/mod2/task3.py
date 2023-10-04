860c3cd1676dab5b20aa2f57ee94a1f46d01de0a
pribishlove <128485621+pribishlove@users.noreply.github.com>
2023-10-04 13:34:08 +0500

    task3.py

--- buns/mod2/task3.py
+++ buns/mod2/task3.py
@@ -1,4 +1,20 @@
-a,b,c = map(int, input().split())
+s = input()
+a = ""
+b = ""
+c = ""
+countSpace = 0
+for i in range(0, len(s)):
+    if s[i]!=" " and countSpace == 0:
+        a += s[i]
+    elif s[i]!=" " and countSpace == 1:
+        b += s[i]
+    elif s[i]!=" " and countSpace == 2:
+        c += s[i]
+    else:
+        countSpace += 1
+a = int(a)
+b = int(b)
+c = int(c)
 if a > b:
     if b > c:
         print(b)
