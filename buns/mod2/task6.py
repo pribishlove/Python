570758abc7e5f0a7937530386e5101345284d3e8
pribishlove <128485621+pribishlove@users.noreply.github.com>
2023-10-04 13:45:26 +0500

    task6.py

--- buns/mod2/task6.py
+++ buns/mod2/task6.py
@@ -1,3 +1,9 @@
-s = input().split(".")
+s = input()
+currentStr = ""
 for i in range (len(s)-1, -1, -1):
-    print(s[i])
+    if s[i] == ".":
+        print(currentStr[::-1])
+        currentStr = ""
+    else:
+        currentStr += s[i]
+print(currentStr)
