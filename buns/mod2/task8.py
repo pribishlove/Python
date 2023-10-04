06e261fe25828163627b6da3b419a78a3c9b88b0
pribishlove <128485621+pribishlove@users.noreply.github.com>
2023-10-04 13:49:53 +0500

    task8.py

--- buns/mod2/task8.py
+++ buns/mod2/task8.py
@@ -1,4 +1,14 @@
-s, i = input().split(", ")
+str = input()
+s = ""
+i = ""
+countSpace = 0
+for k in range(0, len(str)):
+  if str[k]!="," and countSpace == 0:
+    s += str[k]
+  elif str[k]!="," and countSpace == 1:
+    i += str[k]
+  else:
+    countSpace += 1
 count = 0
 for j in s:
     if j == i:
