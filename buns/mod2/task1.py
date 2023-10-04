014307e7253ee1183dcec487d74cc97545401ce3
pribishlove <128485621+pribishlove@users.noreply.github.com>
2023-10-04 13:28:14 +0500

    task1.py

--- buns/mod2/task1.py
+++ buns/mod2/task1.py
@@ -1,2 +1,12 @@
-a,b = map(int, input().split())
+s = input()
+a = ""
+b = ""
+countSpace = 0
+for i in range(0, len(s)):
+  if s[i]!=" " and countSpace == 0:
+    a += s[i]
+  elif s[i]!=" " and countSpace == 1:
+    b += s[i]
+  else:
+    countSpace += 1
 print(a%b)
